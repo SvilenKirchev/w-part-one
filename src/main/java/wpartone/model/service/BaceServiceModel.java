9537f91cf85c51a757f4c03b79a750b742842496
Svilen Kirchev <k_svilen@abv.bg>
2020-06-11 16:36:32 +0300

    init

---
+++ src/main/java/wpartone/model/service/BaceServiceModel.java
@@ -0,0 +1,17 @@
+package wpartone.model.service;
+
+public abstract class BaceServiceModel {
+    private Long id;
+
+    public BaceServiceModel() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/wpartone/model/service/RoleServiceModel.java
@@ -0,0 +1,18 @@
+package wpartone.model.service;
+
+public class RoleServiceModel extends BaceServiceModel{
+
+    private String name;
+
+    public RoleServiceModel() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
